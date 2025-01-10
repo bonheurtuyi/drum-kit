676057f384cda55850378d1fb8e9759f60b9bbca
[Bonheur Tuyikunde] <[bonheurtuyikunde2@gmail.com]>
2025-01-10 12:42:27 +0200

    [Adding click eventListener]

--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,53 @@ numberOfDrumsButton = document.querySelectorAll(".drum");
 
 for (let i = 0; i < numberOfDrumsButton.length; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        let audio = new Audio('../src/sounds/tom-1.mp3');
-        audio.play();
+
+        const buttonInnerHTML = this.innerHTML;
+        switch (buttonInnerHTML){
+            case "w":
+                let snare = new Audio('../src/sounds/snare.mp3');
+                snare.play()
+                break;
+
+            case "a":
+                let crash = new Audio('../src/sounds/crash.mp3');
+                crash.play();
+                break;
+
+            case "s":
+                let kick = new Audio('../src/sounds/kick-bass.mp3');
+                kick.play();
+                break;
+
+            case "d":
+                let tom1 = new Audio('../src/sounds/tom-1.mp3');
+                tom1.play();
+                break;
+
+            case "j":
+                let tom2 = new Audio('../src/sounds/tom-2.mp3');
+                tom2.play();
+                break;
+
+            case "k":
+                let tom3 = new Audio('../src/sounds/tom-3.mp3');
+                tom3.play();
+                break;
+
+            case "l":
+                let tom4 = new Audio('../src/sounds/tom-4.mp3');
+                tom4.play();
+                break;
+
+            default:
+                console.log("invalid key");
+                break;
+        }
     });
 
 }
+
+
+
 
 
